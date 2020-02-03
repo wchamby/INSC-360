0420fcb7a8454468aaa39c1445eed57574f2c22d
wchamby <60372615+wchamby@users.noreply.github.com>
2020-02-03 15:48:18 -0500

    Update homework-2.py

--- homework-2.py
+++ homework-2.py
@@ -1,1 +1,31 @@
-# 
+# (2)(b)
+# William Chase Hamby
+# 2/2/20
+# homework-2.py
+# -*- coding: utf-8 -*-
+# This is the description for homework 2
+
+# (2)(c)
+string_1 = 'This is a test string. '
+string_2 = 'This is also a test string.'
+string_3 = "Knock Knock!\nWho\'s there?\nOrange!\nOrange who?\nOrange you glad I didn\'t say \"banana\"?!"
+number_string = '1234'
+
+# (2)(d)
+print(string_1, string_2, string_3, number_string)
+print(string_1)
+print(string_2)
+print(string_3)
+print(number_string)
+
+#(2)(e)
+joined_string = string_1 + string_2
+print (joined_string)
+
+#(2)(f)
+string_3 = "Knock Knock!\nWho\'s there?\nOrange!\nOrange who?\nOrange you glad I didn\'t say \"banana\"?!"
+print string_3.replace("banana", "grapes")
+
+# (2)(g)
+new_numbers = number_string
+type(number_string)
